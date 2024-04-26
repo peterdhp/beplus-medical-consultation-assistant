f21e03377a6b01247ff69d7cd8c26e8d7e035b9e
peterdhp <36303607+peterdhp@users.noreply.github.com>
2024-04-26 07:07:12 +0000

    yet another p

--- Hello.py
+++ Hello.py
@@ -209,7 +209,7 @@ def advise():
 
 def medical_advisor(medical_record, transcript,openai_api_key):
     rompt_template = """Let's say you are a medical school professor.
-Given a transcript of a patient consultation and a complete medical record written, give medical feedback in Korean.
+Given a transcript of a patient consultation and a complete medical record written, give medical feedback to the doctor in Korean.
 ONLY give feedback that could be critical to the patient, you don't have to say anything if nothing is critical.
 Be as brief as possible.
 
