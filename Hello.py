e359557fe852e0e76295dd9ec745beae62d3408a
peterdhp <36303607+peterdhp@users.noreply.github.com>
2024-04-30 04:21:15 +0000

    advise fix

--- Hello.py
+++ Hello.py
@@ -177,8 +177,8 @@ def call_format():
 
 def advise(): 
     with st.spinner('진료 기록을 검토 및 추정진단을 뽑고 있습니다...'):
-        output = medical_advisor(st.session_state.temp_medical_record,st.session_state.transcript,openai_api_key=openai_api_key)
-    st.session_state.temp_medical_record += '\n\n'+ output
+        output = medical_advisor(st.session_state.temp_medical_record_2,st.session_state.transcript,openai_api_key=openai_api_key)
+    st.session_state.temp_medical_record_2 += '\n\n'+ output
     st.success("진료 내용 검토 성공적으로 완료 되었습니다.")
 
 def medical_advisor(medical_record, transcript,openai_api_key):
